385a73574e53ea11a46e50c77dfc06c67a650309
Jonathan <jono_95@yahoo.com>
2023-03-07 10:36:01 -0600

    Simplified AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,10 @@ require 'pry'
 
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
+
   get "/players" do
     players=Player.all
     players.to_json(include: :agents)
-  end
-
-  get '/players/:id' do
-    player = Player.find(params[:id])
-    player.to_json(include: :agents)
   end
 
   get "/agents" do
